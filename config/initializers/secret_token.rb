03c12e8a28e008832918e29d54076802624e4ee3
austin brennan <austin@myteamsedge.com>
2013-08-29 17:47:14 -0500

    created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oasis::Application.config.secret_token = '06dbca4a2f2437df0e1674d1991c386a99c3e9316f66090d414bcbeaca4f82513eec93758cf7c106daf2a25f70d1e230c3b1524154d2c9c4c79c3b26ed6b5103'
