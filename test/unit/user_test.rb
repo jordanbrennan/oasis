023fc13c33e87bfa1f443247dc900af9771f6e1b
atbrennan <austin@myteamsedge.com>
2013-09-14 13:53:30 -0500

    user_friendship table created

---
+++ test/unit/user_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "a user should enter a first name" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+  
+  test "a user should enter a profile name" do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+  	user = User.new
+  	user.profile_name = users(:austin).profile_name
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile name without spaces" do
+  	user = User.new(first_name: 'austin', last_name: 'brennan', email: 'atbrennan15@hotmail.com')
+    user.password = user.password_confirmation = 'password'
+
+    user.profile_name = "My Profile With Spaces"
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  	assert user.errors[:profile_name].include?("Must be formatted correctly")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'austin', last_name: 'brennan', email: 'atbrennan15@hotmail.com')
+    user.password = user.password_confirmation = 'password'
+
+    user.profile_name = 'atbrennan12'
+    assert user.valid?
+  end
+
+end
